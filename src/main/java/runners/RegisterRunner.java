4a9cd8f540785cb0d51c6c25a23cda62f1be4ac0
chloe-suu <haphuonghph99@gmail.com>
2023-10-31 21:02:38 -0400

    updated

--- src/main/java/runners/RegisterRunner.java
+++ src/main/java/runners/RegisterRunner.java
@@ -6,7 +6,13 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(tags = "@SmokeTest", features = "src/main/java/features/Register.feature", glue = "stepsDef", plugin = {})
+@CucumberOptions(
+		tags = "@SmokeTest", 
+		features = "src/main/java/features/Register.feature", 
+		glue = "stepsDef", 
+		plugin = {"pretty", "json:TestReports/report1.json", "junit:TestReports/report2.xml" ,"html:TestReports/report3.html",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" },
+		publish = true)
 
 public class RegisterRunner {
 
